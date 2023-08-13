96f44345e2ede8e33fb107dc835b9adee6d96a91
Julia Allistair Bayless <julia.allistair@gmail.com>
2023-08-13 14:54:19 -0500

    more clean up

--- app/javascript/packs/index.tsx
+++ app/javascript/packs/index.tsx
@@ -3,7 +3,6 @@
 // of the page.
 
 import React from 'react'
-import ReactDOM from 'react-dom'
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from '../src/App'
